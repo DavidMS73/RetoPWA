0bafded7a31c36ca4598c398d5d1ab08273dc35c
German <gd.martinez@uniandes.edu.co>
2020-11-20 02:15:33 -0500

    Joke

---
+++ jokesapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Joke from "./components/Joke";
+
+function App() {
+  return (
+    <Joke></Joke>
+  );
+}
+
+export default App;
